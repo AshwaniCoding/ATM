c0163fec58813ee37fd1e719ddb3b2b276d6e4cc
Ashwani Kumar Dwivedi <ashwanidwivedi7898@gmail.com>
2024-07-31 00:13:39 +0530

    first commit with JFrame Form

---
+++ src/main/java/com/ashwani/atm/ATM.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.ashwani.atm;
+
+/**
+ *
+ * @author Ashu
+ */
+public class ATM {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MainPage().setVisible(true);
+            }
+        });
+
+    }
+}
